a0db90ff029532ec6a0e625399b4ed66767d5c6a
JoaoDePaulo <joaodepaulocardoso@edu.unifor.br>
2025-09-27 11:41:32 -0300

    feat: successfully create user in database

--- app/[auth]/register/registerAction.ts
+++ app/[auth]/register/registerAction.ts
@@ -1,9 +1,28 @@
 "use server";
 
+import db from "@/lib/db";
+
 export default async function registerAction(formData: FormData) {
   const entries = Array.from(formData.entries());
-  const data = Object.fromEntries(entries);
+  const data = Object.fromEntries(entries) as {
+    name: string;
+    email: string;
+    password: string;
+  };
 
   console.log("=== Register Action ===");
   console.log(data);
+
+  //retornar erro se não tiver preenchido o form
+  if (!data.name || !data.email || !data.password) {
+    throw new Error("Por favor, preencha todos os campos.");
+  }
+
+  await db.user.create({
+    data: {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+    },
+  });
 }
