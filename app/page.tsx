16a6a5bef506c145c85b3b47e388a04a61122afa
JoaoDePaulo <joaodepaulocardoso@edu.unifor.br>
2025-09-27 18:43:05 -0300

    feat: add buttons in home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>hellow world</div>;
+  return (
+    <div className="flex flex-col justify-center items-center space-y-4">
+      <h1 className="text-2xl font-bold">Welcome to the Home Page</h1>
+      <p>Acesse a tela de login:</p>
+      <Button asChild>
+        <a href="/login">Login</a>
+      </Button>
+      <p>Acesse a tela de Cadastro:</p>
+      <Button asChild>
+        <a href="/register">Cadastro</a>
+      </Button>
+    </div>
+  );
 }
