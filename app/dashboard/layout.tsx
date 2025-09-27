653c0511614abcd131abc117434130ebda6ea768
JoaoDePaulo <joaodepaulocardoso@edu.unifor.br>
2025-09-27 18:31:30 -0300

    fix: resolve fix navbar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -7,6 +7,7 @@ export default async function DashboardLayout({
   children: React.ReactNode;
 }) {
   const session = await auth();
+
   if (!session) {
     return redirect("/login");
   }
--- components/navbar-dashboard.tsx
+++ components/navbar-dashboard.tsx
@@ -12,7 +12,9 @@ export function NavbarDashboard() {
   return (
     <div
       className={cn(
-        "mx-auto flex w-full h-screen flex-1 flex-col overflow-auto rounded-md border border-neutral-200 bg-gray-100 md:flex-row dark:border-neutral-700 dark:bg-neutral-800"
+        "mx-auto flex w-full flex-1 flex-col overflow-hidden rounded-md border border-neutral-200 bg-gray-100 md:flex-row dark:border-neutral-700 dark:bg-neutral-800",
+        // for your use case, use `h-screen` instead of `h-[60vh]`
+        "h-screen"
       )}
     >
       <Sidebar open={open} setOpen={setOpen}>
